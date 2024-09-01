647a2f288dff54eb83fe778fc73692b18904e597
Lukas Wutschitz <luwutsch@microsoft.com>
2024-09-01 23:10:47 +0100

    Add some more funcitonality

--- src/paparse/configs.py
+++ src/paparse/configs.py
@@ -1,10 +1,12 @@
 import inspect
+import ruamel.yaml
 
 from dataclasses import fields, asdict, is_dataclass
 from typing import TypeVar, Type
 
 from multimethod import overload
 from ruamel.yaml.comments import CommentedMap
+from pathlib import Path
 
 from .exceptions import ConfigError
 from .converters_primitive import convert
@@ -117,11 +119,87 @@ T = TypeVar('T')
 
 
 class SimpleConfig(AbstractConfig):
+    """
+    A configuration class that provides methods to read from and write to YAML files.
+
+    This class extends the AbstractConfig class and provides additional functionality
+    to handle YAML files, including reading from a YAML file and saving the configuration
+    instance as a YAML file. The class can be converted to and from a dictionary representation
+    which is useful for serialization and deserialization purposes.
+    """
+    
     @classmethod
     def from_yaml_file(cls: Type[T], path: str, context: dict = None) -> T:
+        """
+        Create an instance of the class from a YAML file.
+
+        This method reads a YAML file, optionally processes it with a Jinja2 template
+        using the provided context, and converts it into an instance of the class.
+
+        Args:
+            cls (Type[T]): The class type to instantiate.
+            path (str): The file path to the YAML file.
+            context (dict, optional): A dictionary of context variables to render the 
+                                      Jinja2 template. Defaults to None.
+
+        Returns:
+            T: An instance of the class created from the YAML file data.
+
+        Raises:
+            ConfigError: If there is an issue with the configuration data.
+        """
         return convert(read_templated_yaml_file(path, context=context), cls)
+
+    def save_as_yaml_file(self, path: str):
+        """
+        Convert the configuration instance to a YAML file.
+
+        This method converts the current configuration instance to a dictionary
+        and then writes the dictionary to a YAML file.
+
+        Args:
+            path (str): The file path to the YAML file.
+
+        Raises:
+            ConfigError: If there is an issue with the configuration data.
+        """
+        yaml = ruamel.yaml.YAML()
+        with open(path, 'w', encoding='utf-8') as f:
+            yaml.dump(self.to_dict(), f)
 
 
 @overload
 def convert(data: isa(dict), Config: issubclassof(SimpleConfig)):  # noqa: F811
-    return Config.from_dict(data)
+    """
+    Convert a dictionary to a configuration instance.
+
+    This function converts a given dictionary into an instance of the specified
+    configuration class. The dictionary can be in one of two formats:
+    
+    1. A direct dictionary where all the parameters required by the Config class are present.
+    2. A dictionary with a single element where the key is the name of the config (which must be a subclass of Config)
+       and the value is a dictionary like in the first format.
+
+    Args:
+        data (dict): The dictionary containing configuration data.
+        Config (Type[SimpleConfig]): The configuration class to instantiate.
+
+    Returns:
+        SimpleConfig: An instance of the configuration class created from the dictionary data.
+
+    Raises:
+        ConfigError: If the data cannot be converted to the specified configuration class.
+    """
+    try:
+        return Config.from_dict(data)
+    # TODO only catch specific errors
+    except:
+        pass
+
+    if len(data) == 1:
+        config_name = list(data.keys())[0]
+        config_dict = list(data.values())[0]
+        config_factory = ConfigFactory(Config)
+        return config_factory.create(config_name).from_dict(config_dict)
+    
+    raise ConfigError(f"Cannot convert {data} to {Config}")
